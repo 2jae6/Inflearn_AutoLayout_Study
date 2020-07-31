4a99f79ead4ee12bf465df58f8420e61fb602612
JaeWook Lee <2jae66@gmail.com>
2020-07-31 20:23:41 +0900

    basic_9_multiplier

---
+++ Basic_10_Multiplier_3/Basic_10_Multiplier_3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Basic_10_Multiplier_3
+//
+//  Created by 1 on 2020/07/31.
+//  Copyright © 2020 wook. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Basic_8_Multiplier_1/Basic_8_Multiplier_1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Basic_8_Multiplier_1
+//
+//  Created by 1 on 2020/07/30.
+//  Copyright © 2020 wook. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
