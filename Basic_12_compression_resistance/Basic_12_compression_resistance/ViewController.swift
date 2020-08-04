511b276e26e26b1cfa8c4e2085430e3505958021
JaeWook Lee <2jae66@gmail.com>
2020-08-04 13:17:53 +0900

    Content Hugging Priority and Content Compression Resistance Priority

---
+++ Basic_12_compression_resistance/Basic_12_compression_resistance/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Basic_12_compression_resistance
+//
+//  Created by 1 on 2020/08/04.
+//  Copyright © 2020 wook. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
