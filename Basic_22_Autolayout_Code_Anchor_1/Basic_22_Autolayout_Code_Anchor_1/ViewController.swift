dd524d4c7c61bcef80fa395a898703d768ddfc94
JaeWook Lee <2jae66@gmail.com>
2020-08-25 21:32:13 +0900

    code anchor

---
+++ Basic_22_Autolayout_Code_Anchor_1/Basic_22_Autolayout_Code_Anchor_1/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Basic_22_Autolayout_Code_Anchor_1
+//
+//  Created by 1 on 2020/08/25.
+//  Copyright © 2020 wook. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        let myButton = UIButton.init(type: .system)
+        myButton.setTitle("My Button", for: .normal)
+        
+        self.view.addSubview(myButton)
+        
+        myButton.translatesAutoresizingMaskIntoConstraints = false
+        myButton.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+        myButton.centerYAnchor.constraint(equalTo: self.view.centerYAnchor).isActive = true
+    }
+
+
+}
+
