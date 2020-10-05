e6619605620a8a8dfa905c15b85b625831363b5c
JaeWook Lee <2jae66@gmail.com>
2020-10-05 18:46:18 +0900

    Expand Cell _ 1

--- ExpendCell/ExpendCell/ViewController.swift
+++ ExpendCell/ExpendCell/ViewController.swift
@@ -6,14 +6,34 @@
 //
 
 import UIKit
+class ExpandCell: UITableViewCell{
+    
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+    
+}
 
-class ViewController: UIViewController {
+
+class ViewController: UIViewController,UITableViewDelegate, UITableViewDataSource {
+  
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! ExpandCell
+        
+        cell.descriptionLabel.text = "aaaaa"
+        return cell
+    }
 
+    
 }
 
